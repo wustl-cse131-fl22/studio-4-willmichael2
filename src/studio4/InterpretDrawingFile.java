bc9412ee6f3fc33670352319b54a9e89ef049d13
Will <Will@Will-laptop.dhcp.wustl.edu>
2022-10-12 15:25:21 -0500

    r

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,55 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		double x;
+		double y;
+		double halfWidth;
+		double halfHeight;
+		String shapeType = in.next();
+		int redComponent =in.nextInt();
+		int greenComponent=in.nextInt();
+		int blueComponent=in.nextInt();
+		boolean isFilled=in.nextBoolean();
+		double parameterOne=in.nextDouble();
+		double parameterTwo=in.nextDouble();
+		double parameterThree=in.nextDouble();
+
+		System.out.println(shapeType);
+		System.out.println(parameterThree);
+		StdDraw.setPenColor(redComponent,greenComponent,blueComponent);
+		
+		if(shapeType.equals("rectangle")) {
+			if(isFilled==true) {
+				StdDraw.filledRectangle(parameterOne, parameterTwo, parameterThree, parameterThree);
+			}
+			else {
+				StdDraw.rectangle(parameterOne, parameterTwo, parameterThree, parameterThree);
+			}
+		}
+		
+		if(shapeType.equals("ellipse")) {
+			if(isFilled==true) {
+				StdDraw.filledEllipse(parameterOne, parameterTwo, parameterThree, parameterThree);
+			}
+			else {
+				StdDraw.ellipse(parameterOne, parameterTwo, parameterThree, parameterThree);
+			}
+		}
+		
+		if(shapeType.equals("triangle")) {
+			double parameterFour=in.nextDouble();
+			double parameterFive=in.nextDouble();
+			double parameterSix=in.nextDouble();
+			
+			double[] xValues = {parameterOne, parameterThree, parameterFive};
+			double[] yValues = {parameterTwo, parameterFour, parameterSix};
+			if(isFilled==true) {
+				StdDraw.filledPolygon(xValues,yValues);
+			}
+			else {
+				StdDraw.filledPolygon(xValues,yValues);
+			}
+		}
+
 	}
 }
